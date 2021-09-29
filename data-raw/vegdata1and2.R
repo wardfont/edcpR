d628b53c97df6eddc33d85aeaa0804ab790f0e21
wardfont <fonteyn.ward@gmail.com>
2021-09-29 16:34:46 +0200

    data cleanup

--- data-raw/vegdata1and2.R
+++ data-raw/vegdata1and2.R
@@ -1,7 +1,9 @@
 ## code to prepare `vegdata1` dataset goes here
 
-vegdata1 <- read.csv2("./data-raw/vegdata1.csv")
-vegdata2 <- read.csv2("./data-raw/vegdata2.csv")
+vegdata1 <- read.csv("./data-raw/vegdata1.csv", sep = ";")
+vegdata1$Date <- as.Date(vegdata1$Date, format = "%e.%m.%Y")
+vegdata2 <- read.csv("./data-raw/vegdata2.csv", sep = ";")
+vegdata2$Date <- as.Date(vegdata2$Date, format = "%d/%m/%Y")
 
 usethis::use_data(vegdata1, overwrite = TRUE)
 usethis::use_data(vegdata2, overwrite = TRUE)
